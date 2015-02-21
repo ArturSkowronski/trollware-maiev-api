86d4f7703de30414f482064ac565c811e9d0d25f
arturskowronski <articles.pl@gmail.com>
2015-02-21 11:33:12 +0100

    Leaky abstraction for socket.io

--- index.js
+++ index.js
@@ -3,21 +3,15 @@ var http = require('http');
 var app = express();
 var server = http.createServer(app);
 
-var connection = require('./src/modules/connection')
+var io = require('./src/modules/connection')
+io.initialize(server);
+
+var game = require('./src/modules/game')(io)
 
 app.set('port', (process.env.PORT || 5000));
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World! Test Buildu #2');
-});
 
-app.get('/socket', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
-server.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
-
-connection.initialize(server);
+server.listen(app.get('port'), function() {console.log("Node app is running at localhost:" + app.get('port'));});
+app.get('/', function(request, response) {response.send('Hello World! Test Buildu #2');});
+app.get('/socket', function (req, res) {res.sendFile(__dirname + '/index.html');});
--- src/modules/connection.js
+++ src/modules/connection.js
@@ -1,10 +1,30 @@
 var socketio = require('socket.io');
+var events = require('./events');
+var log = require('winston');
 
-var connection = function(){
-  console.log("Connected");
-}
- 
+var connection ={}
+
 exports.initialize = function (app) {
-	io = socketio(app);	
-	io.on('connection', connection)
+	connection = socketio(app);	
+	log.info("Socket.io Initialized");
+}
+
+exports.createNamespace = function (app) {
+	connection = socketio(app);	
+	connection.on('connection', connection)
+}
+
+exports.emit = function (symbol, data) {
+	events.validateSymbol(symbol);
+	connection.emit('connection', connection);
+}
+
+exports.defineEvent = function (eventName) {
+	return events.defineEvent(eventName);
+}
+
+exports.on = function (symbol, callback) {
+	events.validateSymbol(symbol);
+	log.info("Emmit Event '%s'", Symbol.keyFor(symbol));
+	connection.on(Symbol.keyFor(symbol), callback)
 }
